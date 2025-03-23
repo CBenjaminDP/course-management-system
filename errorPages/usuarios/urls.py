1a40fe21a52ace61861393dfb54b26c8c037db78
CBenjaminDP <benjamin.diaz@entrypay.net>
2025-03-22 22:34:31 -0600

    --amend

--- errorPages/usuarios/urls.py
+++ errorPages/usuarios/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.listar_usuarios, name='listar_usuarios'),
     path('todos/', views.listar_usuarios, name='listar_todos_usuarios'),
     path('registrar/', views.registrar_usuario, name='registrar_usuario'),
-    path('uuid/<uuid:id>/', views.obtener_usuario, name='obtener_usuario_uuid'),
-    path('<int:id>/', views.obtener_usuario, name='obtener_usuario'),
-    path('actualizar/<uuid:id>/', views.actualizar_usuario, name='actualizar_usuario'),
-    path('eliminar/<int:id>/', views.eliminar_usuario, name='eliminar_usuario'),
-    path('desactivar/<uuid:id>/', views.desactivar_usuario, name='desactivar_usuario'),  # New route
+    # Cambiar esta ruta para manejar UUIDs sin guiones
+    path('uuid/<str:id>/', views.obtener_usuario, name='obtener_usuario_uuid'),
+    path('actualizar/<str:id>/', views.actualizar_usuario, name='actualizar_usuario'),
+    path('eliminar/<str:id>/', views.eliminar_usuario, name='eliminar_usuario'),
+    path('desactivar/<str:id>/', views.desactivar_usuario, name='desactivar_usuario'),
 ]
 
